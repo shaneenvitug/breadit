7350e8b1bc18fda6a38e8483cdc4ccb155dbb819
shaneenvitug <shaneenvitug@gmail.com>
2023-06-27 21:29:26 +1000

    add sign up link

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,4 +1,5 @@
 import { Icons } from './Icons'
+import Link from 'next/link'
 
 const SignIn = () => {
   return <div className='container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]'>
@@ -7,6 +8,15 @@ const SignIn = () => {
       <h1 className="text-2xl font-semibold tracking-tight">Welcome back</h1>
       <p className='text-sm max-w-xs mx-auto'>
         By continuing, you are setting up a Breadit account and agree to our User Agreement and Privacy Policy.
+      </p>
+
+      {/* sign in form */}
+
+      <p className='px-8 text-center text-sm text-zinc-700'>
+        New to Breadit?{' '}
+        <Link href='/sign-up' className='hover:text-zinc-800 text-sm underline underline-offset-4'>
+          Sign Up
+        </Link>
       </p>
     </div>
   </div>
